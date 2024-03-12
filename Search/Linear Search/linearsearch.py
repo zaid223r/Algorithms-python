7440bed738a741a480003a4acc462416c2a98ae2
Zaid_Zuhair <zaidzuhairalani@gmail.com>
2024-03-13 02:34:34 +0300

    Add files via upload

---
+++ Search/Linear Search/linearsearch.py
@@ -0,0 +1,16 @@
+
+
+def main():
+    list_of_oddnumbers = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35]
+    target = input("Enter your search target: ")
+    linearsearch(target,list_of_oddnumbers)
+
+def linearsearch(target,list_of_oddnumbers):
+    for i in range(len(list_of_oddnumbers)):
+        if list_of_oddnumbers[i] == int(target):
+            print("The Target {}: It taked {} step/s to find that target".format(target,i))
+            return
+    print("Not Found")
+
+if __name__ == "__main__":
+    main()
